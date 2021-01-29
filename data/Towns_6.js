e7184f50936f3af006a16f86a57a60a45b212dd1
teamdhaksha <63536148+teamdhaksha@users.noreply.github.com>
2021-01-29 15:54:50 +0530

    Add files via upload

---
+++ data/Towns_6.js
@@ -0,0 +1,14 @@
+var json_Towns_6 = {
+"type": "FeatureCollection",
+"name": "Towns_6",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NAME": "Musiri" }, "geometry": { "type": "Point", "coordinates": [ 78.444892053618773, 10.95556313346018, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Lalgudi" }, "geometry": { "type": "Point", "coordinates": [ 78.817161679681575, 10.87189955015362, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Srirangam" }, "geometry": { "type": "Point", "coordinates": [ 78.689493862907284, 10.862335268496629, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Thuvarankurichi" }, "geometry": { "type": "Point", "coordinates": [ 78.386348976114292, 10.375384093916271, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Manapparai" }, "geometry": { "type": "Point", "coordinates": [ 78.422994696450701, 10.60976825734155, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Thuraiyur" }, "geometry": { "type": "Point", "coordinates": [ 78.593210337003555, 11.12231945994022, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Tiruchirappalli" }, "geometry": { "type": "Point", "coordinates": [ 78.681463873926731, 10.81997180073563, -999999.0 ] } }
+]
+}
